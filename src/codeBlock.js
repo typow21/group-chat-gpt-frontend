1bc8511e12ef50709576350c65550b6395d5f6b4
Tyler <tuh26432@temple.edu>
2024-02-27 01:49:08 -0500

    code block update

--- src/codeBlock.js
+++ src/codeBlock.js
@@ -1,24 +1,33 @@
 import React, { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { FiCopy } from "react-icons/fi"; // Assuming FiCopy is imported from react-icons
 
 const CodeBlock = ({ language, code }) => {
   const [copied, setCopied] = useState(false);
 
   const handleCopy = () => {
     setCopied(true);
-    setTimeout(() => setCopied(false), 2000);
+    setTimeout(() => setCopied(false), 3500);
   };
 
   return (
     <div className="code-block-container">
-      <CopyToClipboard text={code} onCopy={handleCopy}>
-        <button className="copy-button">{copied ? "Copied!" : "Copy"}</button>
-      </CopyToClipboard>
-      <div className="code-block">
-        <pre>
+      <br></br>
+      <div className="code-block-header">
+        <div className="language-display">{language}</div>
+        <CopyToClipboard text={code} onCopy={handleCopy}>
+          <div className="copy-button">
+            <FiCopy /> {/* Use FiCopy icon */}
+          </div>
+        </CopyToClipboard>
+      </div>
+      {copied && <div className="copied-label">Copied!</div>}
+      <div className={`code-block language-${language}`}>
+        <pre className="line-numbers">
           <code className={`language-${language}`}>{code}</code>
         </pre>
       </div>
+      
     </div>
   );
 };
